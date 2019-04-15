b2d954e0c8b828fe41639c9a4d4478148b8dda97
Simon Struck <simon@simonscode.org>
2019-04-15 15:11:05 +0200

    Improved field initialisation

--- code/05_OO/src/shapes_main.cpp
+++ code/05_OO/src/shapes_main.cpp
@@ -9,23 +9,17 @@ struct Position {
     int x;
     int y;
 
-    explicit Position(int x_ = 0, int y_ = 0) {
-        x = x_;
-        y = y_;
-    }
+    explicit Position(int x = 0, int y = 0) : x(x), y(y) {}
 };
 
 class Shape {
 protected:
-    Colors _color;
-    std::string _symbol;
-    Position _position;
+    Colors color;
+    std::string symbol;
+    Position position;
 public:
-    explicit Shape(Colors color = Colors::WHITE, Position position = Position(0, 0), std::string symbol = "#") {
-        _color = color;
-        _symbol = std::move(symbol);
-        _position = position;
-    }
+    explicit Shape(Colors color = Colors::WHITE, Position position = Position(0, 0), std::string symbol = "#")
+            : color(color), symbol(symbol), position(position) {}
 
     virtual void draw() {};
 
@@ -37,10 +31,11 @@ public:
 
 class Point : public Shape {
 public:
-    explicit Point(Colors color, int x = 0, int y = 0) : Shape(color, Position(x, y)) {}
+    explicit Point(Colors color, int x = 0, int y = 0)
+            : Shape(color, Position(x, y)) {}
 
     void draw() override {
-        ansiConsole.printText(_position.x, _position.y, "*", _color);
+        ansiConsole.printText(position.x, position.y, "*", color);
     }
 
     double area() override {
@@ -50,87 +45,83 @@ public:
 
 class Circle : public Shape {
 protected:
-    int _radius;
+    int radius;
 public:
-    explicit Circle(Colors color, int x = 0, int y = 0, int radius = 0) : Shape(color, Position(x, y)) {
-        _radius = radius;
-    }
+    explicit Circle(Colors color, int x = 0, int y = 0, int radius = 0)
+            : Shape(color, Position(x, y)), radius(radius) {}
 
     void draw() override {
         /* see https://de.wikibooks.org/wiki/Formelsammlung_Mathematik:_Geometrie
         * Höhensatz des Euklid
         * */
-        int x_start = _position.x - _radius / 2;
-        int x_stop = _position.x + _radius / 2;
+        int x_start = position.x - radius / 2;
+        int x_stop = position.x + radius / 2;
         for (int i = x_start; i <= x_stop; i++) {
             double x_relative = double(i) - double(x_start);
             double h = sqrt(x_relative * (x_stop - x_start - x_relative));
-            ansiConsole.printText(_position.x + int(x_relative) - _radius / 2,
-                                  _position.y + h, _symbol,
-                                  _color);
-            ansiConsole.printText(_position.x + int(x_relative) - _radius / 2,
-                                  _position.y - h, _symbol,
-                                  _color);
+            ansiConsole.printText(position.x + int(x_relative) - radius / 2,
+                                  position.y + h, symbol,
+                                  color);
+            ansiConsole.printText(position.x + int(x_relative) - radius / 2,
+                                  position.y - h, symbol,
+                                  color);
         }
     }
 
     double area() override {
-        return M_PI * M_PI * _radius;
+        return M_PI * M_PI * radius;
     }
 };
 
 class Rectangle : public Shape {
 protected:
-    int _width;
-    int _height;
+    int width;
+    int height;
 public:
-    explicit Rectangle(Colors color, int x = 0, int y = 0, int width = 0, int height = 0) : Shape(color,
-                                                                                                  Position(x, y)) {
-        _width = width;
-        _height = height;
-    }
+    explicit Rectangle(Colors color, int x = 0, int y = 0, int width = 0, int height = 0)
+            : Shape(color, Position(x, y)), width(width), height(height) {}
 
     void draw() override {
-        int x_start = _position.x;
-        int x_stop = _position.x + _height;
+        int x_start = position.x;
+        int x_stop = position.x + height;
 
-        int y_start = _position.y;
-        int y_stop = _position.y + _width;
+        int y_start = position.y;
+        int y_stop = position.y + width;
 
         // draw and bottom
         for (int i = x_start; i <= x_stop; i++) {
-            ansiConsole.printText(i, y_start, _symbol, _color);
-            ansiConsole.printText(i, y_stop, _symbol, _color);
+            ansiConsole.printText(i, y_start, symbol, color);
+            ansiConsole.printText(i, y_stop, symbol, color);
         }
 
         // draw sides
         for (int i = y_start; i <= y_stop; i++) {
-            ansiConsole.printText(x_start, i, _symbol, _color);
-            ansiConsole.printText(x_stop, i, _symbol, _color);
+            ansiConsole.printText(x_start, i, symbol, color);
+            ansiConsole.printText(x_stop, i, symbol, color);
         }
     }
 
     double area() override {
-        return _height * _width;
+        return height * width;
     }
 };
 
 class Scene {
 protected:
-    std::vector<Shape *> _shapes;
+    std::vector<Shape *> shapes;
 public:
     void add(Shape *shape) {
-        _shapes.push_back(shape);
+        shapes.push_back(shape);
     }
 
     void draw() {
-        for (Shape *shape : _shapes) {
+        for (Shape *shape : shapes) {
             shape->draw();
         }
     }
 
     ~Scene() {
-        for (Shape *shape : _shapes) {
+        for (Shape *shape : shapes) {
             delete shape;
         }
     }
