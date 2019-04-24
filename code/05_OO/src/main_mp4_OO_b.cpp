9b5cbb0504a18f833312fde7cd6a0f0adfe052bc
Simon Struck <simon@simonscode.org>
2019-04-24 14:08:17 +0200

    Fix last tasks for 05_b

--- code/05_OO/src/main_mp4_OO_b.cpp
+++ code/05_OO/src/main_mp4_OO_b.cpp
@@ -5,20 +5,24 @@
 //=======================================
 
 struct FooVal {
-    FooVal(int initialValue=0);
-    ~FooVal();
+  FooVal(int initialValue = 0);
+
+  ~FooVal();
+
 private:
-    int _someValue;
+  int _someValue;
 };
 
 //=======================================
 
 class FooBase {
-    FooVal _value;
+  FooVal _value;
 public:
-    FooBase();
-    FooBase(int initialValue);
-    virtual ~FooBase();
+  FooBase();
+
+  FooBase(int initialValue);
+
+  virtual ~FooBase();
 };
 
 //=======================================
@@ -26,156 +30,188 @@ public:
 class FooDerived : public FooBase {
   int _ival;
 public:
-    FooDerived(int initialValue);
-    ~FooDerived();
+  FooDerived(int initialValue);
+
+  ~FooDerived();
 };
 
 //=======================================
 
 class Bar {
-    FooBase *_helperObject;
+  FooBase *_helperObject;
 public:
-    Bar();
-    ~Bar();
+  Bar();
+
+  ~Bar();
 };
 
 //=======================================
 
 FooVal::FooVal(int initialValue)
-: _someValue(initialValue)
-{
-    std::cout << "FooVal::FooVal()" << std::endl;
+  : _someValue(initialValue) {
+  std::cout << "FooVal::FooVal()" << std::endl;
 }
 
-FooVal::~FooVal(){
-    std::cout << "FooVal::~FooVal()" << std::endl;
+FooVal::~FooVal() {
+  std::cout << "FooVal::~FooVal()" << std::endl;
 }
 
-FooBase::FooBase(){
-    std::cout << "FooBase::FooBase()" << std::endl;
+FooBase::FooBase() {
+  std::cout << "FooBase::FooBase()" << std::endl;
 }
 
 FooBase::FooBase(int initialValue)
-: _value(initialValue)
-{
-    std::cout << "FooBase::FooBase(int)" << std::endl;
+  : _value(initialValue) {
+  std::cout << "FooBase::FooBase(int)" << std::endl;
 }
 
-FooBase::~FooBase(){
-    std::cout << "FooBase::~FooBase(" << std::endl;
+FooBase::~FooBase() {
+  std::cout << "FooBase::~FooBase(" << std::endl;
 }
 
 FooDerived::FooDerived(int initialValue)
-: FooBase(initialValue), _ival(0)
-{
-    std::cout << "FooDerived::FooDerived()" << std::endl;
+  : FooBase(initialValue), _ival(0) {
+  std::cout << "FooDerived::FooDerived()" << std::endl;
 }
 
-FooDerived::~FooDerived(){
-    std::cout << "FooDerived::~FooDerived()" << std::endl;
+FooDerived::~FooDerived() {
+  std::cout << "FooDerived::~FooDerived()" << std::endl;
 }
 
-Bar::Bar(){
-    _helperObject =  new FooDerived(0);
+Bar::Bar() {
+  _helperObject = new FooDerived(0);
 }
 
-Bar::~Bar(){
-    delete _helperObject;
+Bar::~Bar() {
+  delete _helperObject;
 }
 
 
 struct StackObject {
 private:
-    void* operator new(size_t size) noexcept {
-        bool noStackObjectOnHeap = false;
-        assert(noStackObjectOnHeap);
-        return nullptr;
-    }
+  void *operator new(size_t size) noexcept {
+    bool noStackObjectOnHeap = false;
+    assert(noStackObjectOnHeap);
+    return nullptr;
+  }
 };
 
 
 struct A : public StackObject {
-    A(){std::cout << "+A ";}
-    //A(const A&){std::cout << "+A";}
-    ~A(){std::cout << "-A ";}
+  A() { std::cout << "+A "; }
+
+  //A(const A&){std::cout << "+A";}
+  ~A() { std::cout << "-A "; }
 };
 
 struct B : public StackObject {
-    B(){std::cout << "+B ";}
-    //B(const B&){std::cout << "+B";}
-    ~B(){std::cout << "-B ";}
+  B() { std::cout << "+B "; }
+
+  //B(const B&){std::cout << "+B";}
+  ~B() { std::cout << "-B "; }
 };
 
 
 struct C : public StackObject {
-    C(){std::cout << "+C ";}
-    //C(const C&){std::cout << "+C";}
-    ~C(){std::cout << "-C ";}
+  C() { std::cout << "+C "; }
+
+  //C(const C&){std::cout << "+C";}
+  ~C() { std::cout << "-C "; }
 };
 
-class HeapObject{
+class HeapObject {
 public:
-    void*   operator new (size_t size);
-    HeapObject();
-    virtual ~HeapObject();
-    static bool assertionsHold();
+  void *operator new(size_t size);
+
+  HeapObject();
+
+  virtual ~HeapObject();
+
+  static bool assertionsHold();
+
 protected:
 private:
-    static int ctorCount;
-    static int dtorCount;
-    static int newCount;
-    //    static void remove(HeapObject *);
-    HeapObject(const HeapObject&) = delete;
-    HeapObject& operator=(const HeapObject&) = delete;
+  static int ctorCount;
+  static int dtorCount;
+  static int newCount;
+
+  //    static void remove(HeapObject *);
+  HeapObject(const HeapObject &) = delete;
+
+  HeapObject &operator=(const HeapObject &) = delete;
 };
 
 int HeapObject::ctorCount = 0;
 int HeapObject::dtorCount = 0;
-int HeapObject::newCount  = 0;
+int HeapObject::newCount = 0;
 
-void*   HeapObject::operator new (size_t size){
-    newCount++;
-    return new char[size];
+void *HeapObject::operator new(size_t size) {
+  newCount++;
+  return new char[size];
 }
 
-HeapObject::HeapObject(){
-    ctorCount++;
+HeapObject::HeapObject() {
+  ctorCount++;
 }
 
-HeapObject::~HeapObject(){
-    dtorCount++;
+HeapObject::~HeapObject() {
+  dtorCount++;
 }
 
-bool HeapObject::assertionsHold(){
-    assert(ctorCount == newCount);   // all objects have been allocated on heap
-    assert(ctorCount == dtorCount);  // all objects have been deleted
-    return true;
+bool HeapObject::assertionsHold() {
+  assert(ctorCount == newCount);   // all objects have been allocated on heap
+  assert(ctorCount == dtorCount);  // all objects have been deleted
+  return true;
 }
 
 
-class K : public HeapObject
-{
+class K : public HeapObject {
 public:
-  K(){std::cout << "+K ";}
-    ~K(){std::cout << "-K ";}
+  K() { std::cout << "+K "; }
+
+  ~K() { std::cout << "-K "; }
 };
 
 class M : public K {
-  private:
-    B b;
 public:
-    M(){std::cout << "+M ";}
-    ~M(){std::cout << "-M ";}
+  M() { std::cout << "+M "; }
+
+  ~M() { std::cout << "-M "; }
 };
 
 class L : public M {
 public:
-  L(){std::cout << "+L ";}
-  ~L(){std::cout << "-L ";}
+  L() { std::cout << "+L "; }
+
+  ~L() { std::cout << "-L "; }
 };
 
 
-void pattern1(){
+class K5 : public HeapObject {
+public:
+  K5() { std::cout << "+K "; }
+
+  ~K5() { std::cout << "-K "; }
+};
+
+class M5 : public K5 {
+private:
+  B b;
+public:
+  M5() { std::cout << "+M "; }
+
+  ~M5() { std::cout << "-M "; }
+};
+
+class L5 : public M5 {
+public:
+  L5() { std::cout << "+L "; }
+
+  ~L5() { std::cout << "-L "; }
+};
+
+
+void pattern1() {
   B b;
   {
     C c;
@@ -183,20 +219,21 @@ void pattern1(){
   A a;
 }
 
-void pattern2(){
+void pattern2() {
   B b;
   C c;
   A a;
 }
 
-void AtoB(A& a) {
+B AtoB(A &a) {
   A ax;
   B b;
+  return b;
 }
 
 // +A(+A+B-B-A)+C-C-A
 
-void pattern3(){
+void pattern3() {
   A a;
   AtoB(a);
   C c;
@@ -204,24 +241,24 @@ void pattern3(){
 
 
 void aufg4() {
-  L* p = new L();
+  L *p = new L();
   delete p;
 }
 
 
-void aufg5(){
+void aufg5() {
+  L5 *p = new L5();
+  delete p;
+}
+
+
+void aufg6() {
 
 }
 
 
-void aufg6(){
-
-}
-
-
-
-int main(int argc, const char * argv[]) {
-  Bar * bar = new Bar();
+int main(int argc, const char *argv[]) {
+  Bar *bar = new Bar();
   // ...
   delete bar; // implies "delete _helperObject";
 
@@ -230,7 +267,7 @@ int main(int argc, const char * argv[]) {
 
   {
     C c;
-    K* k = new K();
+    K *k = new K();
     delete k;
   }
 
@@ -254,11 +291,10 @@ int main(int argc, const char * argv[]) {
   // aufgabe 6
   aufg6();
   std::cout << std::endl;
-  
 
 
   std::cout << " ENDE" << std::endl;
-  std::cin.ignore(); 
+//  std::cin.ignore();
   return 0;
 }
 
--- code/05_OO/src/shapes_main.cpp
+++ code/05_OO/src/shapes_main.cpp
@@ -6,159 +6,189 @@
 #include "AnsiConsole.h"
 
 struct Position {
-    int x;
-    int y;
+  int x;
+  int y;
 
-    explicit Position(int x = 0, int y = 0) : x(x), y(y) {}
+  explicit Position(int x = 0, int y = 0) : x(x), y(y) {}
 };
 
 class Shape {
+private:
+  Position position;
+  std::string symbol;
 protected:
-    Colors color;
-    std::string symbol;
-    Position position;
+  int getX() {
+    return position.x;
+  }
+  int getY() {
+    return position.y;
+  }
+  std::string* getSymbol(){
+    return &symbol;
+  }
+
+  virtual void print(int x, int y){
+    ansiConsole.printText(position.x + x, position.y + y, symbol, Colors::WHITE);
+  }
+
+  virtual void print(int x, int y, std::string text){
+    ansiConsole.printText(position.x + x, position.y + y, text, Colors::WHITE);
+  }
 public:
-    explicit Shape(Colors color = Colors::WHITE, Position position = Position(0, 0), std::string symbol = "#")
-            : color(color), symbol(symbol), position(position) {}
+  explicit Shape(Position position, std::string symbol) : position(position), symbol(symbol) {
 
-    virtual void draw() {};
+  }
 
-    virtual double area() = 0;
+  virtual void draw() = 0;
 
-    virtual ~Shape() = default;
+  virtual double area() = 0;
+
+  virtual ~Shape() = default;
+};
+
+class ColoredShape : public Shape {
+private:
+  Colors color;
+protected:
+  void print(int x, int y) override {
+    ansiConsole.printText(getX() + x, getY() + y, *getSymbol(), color);
+  }
+  virtual void print(int x, int y, std::string text){
+    ansiConsole.printText(getX() + x, getY() + y, text, Colors::WHITE);
+  }
+
+public:
+  explicit ColoredShape(Colors color = Colors::WHITE, Position position = Position(0, 0), std::string symbol = "#")
+    : Shape(position, symbol), color(color) {}
+
+  virtual void draw() = 0;
+
+  virtual double area() = 0;
+
+  virtual ~ColoredShape() = default;
 };
 
 
-class Point : public Shape {
+class Point : public ColoredShape {
 public:
-    explicit Point(Colors color, int x = 0, int y = 0)
-            : Shape(color, Position(x, y), "*") {}
+  explicit Point(Colors color, int x = 0, int y = 0)
+    : ColoredShape(color, Position(x, y), "*") {}
 
-    void draw() override {
-        ansiConsole.printText(position.x, position.y, "*", color);
-    }
+  void draw() override {
+    print(0, 0, "*");
+  }
 
-    double area() override {
-        return 0.0;
-    }
+  double area() override {
+    return 0.0;
+  }
 };
 
-class Circle : public Shape {
+class Circle : public ColoredShape {
 protected:
-    int radius;
+  int radius;
 public:
-    explicit Circle(Colors color, int x = 0, int y = 0, int radius = 0)
-            : Shape(color, Position(x, y)), radius(radius) {}
+  explicit Circle(Colors color, int x = 0, int y = 0, int radius = 0)
+    : ColoredShape(color, Position(x, y)), radius(radius) {}
 
-    void draw() override {
-        /* see https://de.wikibooks.org/wiki/Formelsammlung_Mathematik:_Geometrie
-        * Höhensatz des Euklid
-        * */
-        int x_start = position.x - radius / 2;
-        int x_stop = position.x + radius / 2;
-        for (int i = x_start; i <= x_stop; i++) {
-            double x_relative = double(i) - double(x_start);
-            double h = sqrt(x_relative * (x_stop - x_start - x_relative));
-            ansiConsole.printText(position.x + int(x_relative) - radius / 2,
-                                  position.y + h, symbol,
-                                  color);
-            ansiConsole.printText(position.x + int(x_relative) - radius / 2,
-                                  position.y - h, symbol,
-                                  color);
-        }
+  void draw() override {
+    /* see https://de.wikibooks.org/wiki/Formelsammlung_Mathematik:_Geometrie
+    * Höhensatz des Euklid
+    * */
+    int x_start = getX() - radius / 2;
+    int x_stop = getX() + radius / 2;
+    for (int i = x_start; i <= x_stop; i++) {
+      double x_relative = double(i) - double(x_start);
+      double h = sqrt(x_relative * (x_stop - x_start - x_relative));
+      print(int(x_relative) - radius / 2, h);
+      print(int(x_relative) - radius / 2, h);
     }
+  }
 
-    double area() override {
-        return M_PI * radius * radius;
-    }
+  double area() override {
+    return M_PI * radius * radius;
+  }
 };
 
-class Rectangle : public Shape {
+class Rectangle : public ColoredShape {
 protected:
-    int height;
-    int width;
+  int height;
+  int width;
 public:
-    explicit Rectangle(Colors color, int x = 0, int y = 0, int height = 0, int width = 0)
-            : Shape(color, Position(x, y)), height(height), width(width) {}
+  explicit Rectangle(Colors color, int x = 0, int y = 0, int height = 0, int width = 0)
+    : ColoredShape(color, Position(x, y)), height(height), width(width) {}
 
-    void draw() override {
-        int x_start = position.x;
-        int x_stop = position.x + width;
+  void draw() override {
 
-        int y_start = position.y;
-        int y_stop = position.y + height;
-
-        // draw and bottom
-        for (int i = x_start; i <= x_stop; i++) {
-            ansiConsole.printText(i, y_start, symbol, color);
-            ansiConsole.printText(i, y_stop, symbol, color);
-        }
-
-        // draw sides
-        for (int i = y_start; i <= y_stop; i++) {
-            ansiConsole.printText(x_start, i, symbol, color);
-            ansiConsole.printText(x_stop, i, symbol, color);
-        }
+    // draw and bottom
+    for (int i = 0; i <= width; i++) {
+      print(i, 0);
+      print(i, height);
     }
 
-    double area() override {
-        return width * height;
+    // draw sides
+    for (int i = 0; i <= height; i++) {
+      print(0, i);
+      print(width, i);
     }
+  }
+
+  double area() override {
+    return width * height;
+  }
 };
 
 
-class TextBox : public Rectangle {
+class Sign : public Rectangle {
 protected:
-    std::string text;
+  std::string text;
 public:
-    explicit TextBox(Colors color, std::string text, int x, int y, int height, int width)
-            : Rectangle(color, x, y, height, width), text(text) {}
+  explicit Sign(Colors color, std::string text, int x, int y, int height, int width)
+    : Rectangle(color, x, y, height, width), text(text) {}
 
-    void draw() override {
-        Rectangle::draw();
-        Position text_position = Position(position.x + 4, position.y + height / 2);
+  void draw() override {
+    Rectangle::draw();
 
-        ansiConsole.printText(text_position.x, text_position.y, text, color);
-    }
+    print(4, height/2, text);
+  }
 };
 
 class Scene {
 protected:
-    std::vector<Shape *> shapes;
+  std::vector<ColoredShape *> shapes;
 public:
-    void add(Shape *shape) {
-        shapes.push_back(shape);
-    }
+  void add(ColoredShape *shape) {
+    shapes.push_back(shape);
+  }
 
-    void draw() {
-        for (Shape *shape : shapes) {
-            shape->draw();
-        }
+  void draw() {
+    for (ColoredShape *shape : shapes) {
+      shape->draw();
     }
+  }
 
-    ~Scene() {
-        for (Shape *shape : shapes) {
-            delete shape;
-        }
+  ~Scene() {
+    for (ColoredShape *shape : shapes) {
+      delete shape;
     }
+  }
 };
 
 int main(int argc, char **argv) {
-    ansiConsole.clearScreen();
+  ansiConsole.clearScreen();
 
-    Scene *schneeMann = new Scene();
+  Scene *schneeMann = new Scene();
 
-    schneeMann->add(new Circle(Colors::GREEN, 20, 22, 10));
-    schneeMann->add(new Circle(Colors::GREEN, 20, 15, 6));
-    schneeMann->add(new Circle(Colors::GREEN, 20, 10, 4));
-    schneeMann->add(new TextBox(Colors::GREEN, "Test", 32, 23, 4, 12));
-    schneeMann->add(new Point(Colors::GREEN, 10, 10));
-    schneeMann->add(new Point(Colors::GREEN, 40, 10));
+  schneeMann->add(new Circle(Colors::GREEN, 20, 22, 10));
+  schneeMann->add(new Circle(Colors::GREEN, 20, 15, 6));
+  schneeMann->add(new Circle(Colors::GREEN, 20, 10, 4));
+  schneeMann->add(new Sign(Colors::GREEN, "Test", 32, 23, 4, 12));
+  schneeMann->add(new Point(Colors::GREEN, 10, 10));
+  schneeMann->add(new Point(Colors::GREEN, 40, 10));
 
-    schneeMann->draw();
+  schneeMann->draw();
 
-    std::cin.ignore();
+  std::cin.ignore();
 
-    delete schneeMann;
-    return 0;
+  delete schneeMann;
+  return 0;
 }
